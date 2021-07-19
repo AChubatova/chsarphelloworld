43de49978f7b50296944dc2c3275725abf47c5e0
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-07-19 10:43:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello MVPs!!!!!");
+            Console.WriteLine2("Hello MVPs!!!!!");
         }
     }
 }
